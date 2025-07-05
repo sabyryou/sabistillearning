2e24ff9b1d55c87d45d7f60342327a7e2ddc3500
sabyryou <sakhibarraq12@gmail.com>
2025-07-05 22:13:22 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+function tampikanui(pisanggoreng) {
+    const ui = document.getElementById("urnotalone");
+    if (ui.style.display == "none") {
+        ui.style.display = "block";
+    } else {
+         ui.style.display = "none";
+    }
+
+}
