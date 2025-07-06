c5f27669bc1268b29350e1fd5b7a5641a5dcc3cb
sabyryou <sakhibarraq12@gmail.com>
2025-07-07 03:46:28 +0700

    Add files via upload

--- function.js
+++ function.js
@@ -1,12 +1,10 @@
 function tampikanui(pisanggoreng) {
     const teks = document.getElementById("urnotalone");
-    const gambar = document.getElementsByClassName("bg4");
+
     if (teks.style.display == "none") {
         teks.style.display = "block";
-        gambar.style.display = "block"
     } else {
          teks.style.display = "none";
-         gambar.style.display = "none"
     }
 
 }
